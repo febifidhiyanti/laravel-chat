8013438818868f41f76daa5e64a19ecad1714015
febi fidhiyanti <6358877-Kikok0102@users.noreply.gitlab.com>
2020-09-09 14:54:57 +0700

    by febi

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,22 +2,6 @@
 @section('content')
 
  
-    <div class="container">
-        <div class="col-md-6 ">
-            <div class="panel panel-danger">
-                <div class="panel-heading">Chats</div>
-                    <div class="panel-body">
-                        <chat-messages :messages="messages"></chat-messages>
-                    </div>
-                    <div class="panel-footer">
-                        <chat-form
-                            v-on:messagesent="addMessage"
-                            :user="{{ Auth::user() }}"
-                        ></chat-form>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="col-md-6 pull-right ">
             <div class="panel panel-danger">
                 <div class="panel-heading">Chats</div>
